07befa4ac64342a62e755428adc2e6c3360d097a
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-01-10 12:01:29 +0000

    Product CRUD done

---
+++ models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let productSchema = new Schema(
+    {
+        name: {type:String},
+        description: {type:String},
+        price: {type:Number},
+        inStock: {type:Boolean}
+    }
+)
+
+module.exports = mongoose.model("product",productSchema);
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productRoutes = require("./routes/product");
+const app = express();
+
+require("dotenv-flow").config();
+
+app.use(bodyParser.json());
+
+mongoose.connect(
+    process.env.DBHOST,
+    {
+        //useUnifiedTopology:true,
+        //useNewUrlParser: true
+    }
+).catch(error => console.log("Error connecting to MongoDB:" + error));
+
+mongoose.connection.once("open", () => console.log("Connected succesfully to MongoDB"));
+
+
+// route
+app.get("/api/welcome", (req, res) => {
+    res.status(200).send({message: "Welcome to the MEN REST API"});
+})
+
+app.use("/api/products", productRoutes);
+
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function() {
+    console.log("Server is running on port: " + PORT);
+})
+
+
+
+module.exports = app;
