2d682f0f74b013b89f87a4ecad8d2a3ddd2e84d0
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-04-17 14:20:31 +0200

    Update env files etc.

--- src/util/DBManager.ts
+++ src/util/DBManager.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 export const DBConnect = () => {
-    mongoose.connect(
-        process.env.DBHOST!,
-        {
-            //useUnifiedTopology:true,
-            //useNewUrlParser: true 
-        }
-    ).catch(error => console.log("Error connecting to MongoDB:" + error));
-    
-    mongoose.connection.once("open", () => console.log("Connected succesfully to MongoDB (" + process.env.DBHOST! + ")"));
+  mongoose
+    .connect(process.env.DBHOST!, {
+      //useUnifiedTopology:true,
+      //useNewUrlParser: true
+    })
+    .catch((error) => console.log("Error connecting to MongoDB:" + error));
+
+  mongoose.connection.once("open", () =>
+    console.log("Connected succesfully to MongoDB")
+  );
 };
