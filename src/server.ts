4b668fe925eb9a852da952cd4c52c5f6a64176e0
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-04-18 11:12:07 +0200

    Test update 2

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,14 @@ import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./util/swagger_output.json";
 
-// test
+// test2345
 // Project imports
 import routes from "./routes";
 import { DBConnect } from "./util/DBManager";
 
 require("dotenv-flow").config();
 
-const PORT: Number = parseInt(process.env.PORT as string, 10) || 4000;
+const PORT: Number = parseInt(process.env.PORT as string, 10) || 4000;
 const app: Application = express();
 app.use(bodyParser.json());
 
@@ -22,8 +22,7 @@ DBConnect();
 app.use("/api/", routes);
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
-
 // Start server and listen for request on selected port
-app.listen(PORT, function() {
-    console.log("Server is running on port: " + PORT);
-})
+app.listen(PORT, function () {
+  console.log("Server is running on port: " + PORT);
+});
