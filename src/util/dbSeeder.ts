25d30cd4331533868a0dabbcb7c4852a379a5d54
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-06-21 12:37:24 +0200

    Fix dbSeeder

--- src/util/dbSeeder.ts
+++ src/util/dbSeeder.ts
@@ -1,13 +1,13 @@
 import { productModel } from "../models/productModel";
 import { userModel } from "../models/userModel";
-import { connect } from "./dbManager";
+import { DBManager } from "./DBManager";
 import bcrypt from "bcrypt";
 
 require("dotenv-flow").config();
 
 export const seed = async () => {
   try {
-    connect();
+    DBManager.connect();
 
     await deleteAllData();
     await seedData();
