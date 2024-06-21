f30cba4d38361997e7db966de7e34c2a1b2d0db9
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-06-21 12:33:15 +0200

    Transition to Swagger with JSDocs

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,7 @@
 import express, { Application } from "express";
 import bodyParser from "body-parser";
 import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 import YAML from "yamljs";
 import cors from "cors";
 
@@ -35,12 +36,87 @@ export class App {
    *
    */
   private setupSwagger() {
-    const swaggerDocument = YAML.load("./docs/swagger.yaml");
-    this.app.use(
-      "/api/docs",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocument)
-    );
+    const swaggerDefinition = {
+      openapi: "3.0.0",
+      info: {
+        title: "Express API for JSONPlaceholder",
+        version: "1.0.0",
+        description:
+          "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
+        license: {
+          name: "Licensed Under MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "JSONPlaceholder",
+          url: "https://jsonplaceholder.typicode.com",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:4000/api/",
+          description: "Development server"
+        }
+      ],
+
+      tags: [
+        {
+          name: "Product Routes",
+          description: "Routes that handles products"
+        },
+        {
+            name: "User Routes",
+            description: "Routes that handles users"
+        }
+      ],
+
+      components: {
+
+        securitySchemes: {
+            ApiKeyAuth: {
+                type: "apiKey",
+                in: "header",
+                name: "auth-token"
+            }            
+        },           
+        schemas: 
+            {
+                Product: {
+                    type: "object",
+                    properties: {
+                        id: { type: "string" },
+                        name: { type: "string" },
+                        description: { type: "string" },
+                        price: { type: "number" },
+                        stock: { type: "number" },
+                        status: { type: "boolean" },
+                        _createdBy: { type: "string" }
+                    }
+                },
+                User: {
+                    type: "object",
+                    properties: {
+                        id: { type: "string" },
+                        name: { type: "string" },
+                        email: { type: "string" },
+                        password: { type: "string" },
+                        registerDate: { type: "string" }
+                    }
+                }
+            }
+        }      
+    };
+
+    const options = {
+      swaggerDefinition,
+      // Paths to files containing OpenAPI definitions
+      apis: ["**/*.ts"],
+    };
+
+    const swaggerSpec = swaggerJSDoc(options);
+
+    //const swaggerDocument = YAML.load("./docs/swagger.yaml");
+    this.app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   }
 
   /**
--- src/routes.ts
+++ src/routes.ts
@@ -10,7 +10,7 @@ import {
 } from "./controllers/productController";
 
 //import { ProductControllerC } from "./controllers/ProductControllerC";
-
+//
 import {
   registerUser,
   loginUser,
@@ -19,21 +19,117 @@ import {
 
 const router: Router = Router();
 
-// healthcheck
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     tags:
+ *       - App Routes
+ *     summary: Health check
+ *     description: Basic route to check if the api is running
+ *     responses:
+ *       200:
+ *         description: Server up and running.
+ */
 router.get("/", (req: Request, res: Response) => {
-  res.status(200).send({ message: "API running" });
-});
-
-// healthcheck
-router.get("/welcome", (req: Request, res: Response) => {
   res.status(200).send({ message: "Welcome to the TypeScript MEN REST-API" });
 });
 
-// auth/user routes
+/**
+ * @swagger
+ * /user/register:
+ *   post:
+ *     tags:
+ *       - User Routes
+ *     summary: Register a new user
+ *     description: Takes a user in the body and tries to register it in the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/User"
+ *     responses:
+ *       201:
+ *         description: User created succesfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 _id:
+ *                   type: string
+ */
 router.post("/user/register", registerUser);
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     tags:
+ *       - User Routes
+ *     summary: Login an existing user
+ *     description: Logs in an existing user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User logged in succesfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ */
 router.post("/user/login", loginUser);
 
-// product routes
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     tags:
+ *       - Product Routes
+ *     summary: Create a new Product
+ *     description: Create a new Product
+ *     security:
+ *       - ApiKeyAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Product"
+ *           example:
+ *             name: "Test Product"
+ *             description: "Test Product description"
+ *             price: 10.95
+ *             stock: 10
+ *             status: false
+ *             _createdBy: ""
+ *
+ *     responses:
+ *       201:
+ *         description: Product created succesfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/Product"
+ */
 router.post("/products", createProduct);
 router.get("/products", getAllProducts);
 router.get("/products/instock", getProductsInStock);
