4be21e214b680608f1dd82d40df36faa21799b44
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2024-12-13 17:00:43 +0100

    Added cron-job with auto-timeout for 2 hours as default

--- src/controllers/devToolsController.ts
+++ src/controllers/devToolsController.ts
@@ -6,36 +6,57 @@ import {
 import cron from "node-cron";
 import https from "https";
 
-const timeOutSeconds = 60 * 60 * 2;
+let durationMinutes = 120;
+const cronPattern = "*/5 * * * *";
 
-const task = cron.schedule('*/5 * * * *', () => {
+/**
+ * 
+ */
+const task = cron.schedule(cronPattern, () => {
 
+  /**
+   * 
+   */
   https.get('https://ments-restapi.onrender.com/api/docs/', () => {
     console.log('Pinged the server');
+    durationMinutes--;
+
+    if (durationMinutes <= 0) {
+      task.stop();
+      console.log('Stopped the cron job due to inactivity');
+    }
   });
-
-
 });
 
+/**
+ * 
+ * @param req 
+ * @param res 
+ */
 export async function startCron(req: Request, res: Response) {
 
   try {
 
     task.start();
 
-    res.status(200).send("Started cron-job");
+    res.status(200).send("Started cron-job from active route call");
   } catch (error) {
     res.status(500).send({ message: error });
   }
 };
 
 
+/**
+ * 
+ * @param req 
+ * @param res 
+ */
 export async function stopCron(req: Request, res: Response) {
   try {
 
     task.stop();
 
-    res.status(200).send("Stopped cron-job");
+    res.status(200).send("Stopped the cron-job from active route call");
   } catch (error) {
     res.status(500).send({ message: error });
   }
