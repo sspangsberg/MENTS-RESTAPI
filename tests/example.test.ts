ecaf0540e33ef1144e9a23052952c45e1e431621
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-02-18 17:00:05 +0100

    Update workflow file

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("API workflow tests", () => {
     test("To get all products", async ({ request }) => {
-        test.setTimeout(45000);
+        
         const response = await request.get("/api/products");
         const result = await response.json();
 
