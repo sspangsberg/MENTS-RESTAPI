37f9ad73ac270c988f5d64dda1570330e8009865
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-05 14:52:59 +0100

    Workflow typo and example test fix

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,11 +2,11 @@ import { test, expect } from "@playwright/test";
 
 test.describe("API workflow tests", () => {
     test("To get all products", async ({ request }) => {
-        
+
         const response = await request.get("/api/products");
         const result = await response.json();
 
-        console.log(result);
+        //console.log(result);
 
         expect(response.ok()).toBeTruthy();
         expect(response.status()).toBe(200);
