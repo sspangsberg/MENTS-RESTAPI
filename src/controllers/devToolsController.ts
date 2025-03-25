c23a48e776950941cd4e4b66b66f70c6df188141
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-25 15:22:46 +0100

    Update devToolController with comments etc.

--- src/controllers/devToolsController.ts
+++ src/controllers/devToolsController.ts
@@ -6,19 +6,20 @@ import {
 import cron from "node-cron";
 import https from "https";
 
-let durationMinutes = 120;
-const MINUTES_DELTA = 5;
-const cronPattern = "*/" + MINUTES_DELTA + " * * * *";
+// Settings
+let durationMinutes = 120; // Total "keep-remote-server-alive" duration 
+const MINUTES_DELTA = 5; // How often should we ping the server
+const cronPattern = "*/" + MINUTES_DELTA + " * * * *"; // Docs here: https://crontab.guru/#*/5_*_*_*_*
 const URL = "https://ments-restapi.onrender.com/api/docs/";
 
 
 /**
- * 
+ * Initialize the task with the specified cronPattern
  */
 const task = cron.schedule(cronPattern, () => {
 
     /**
-   * 
+   * Ping the server URL using a GET request. Stop if duration is 0 or less
    */
     https.get(URL, () => {
         console.log('Pinged the server');
