3710d11f86767d6c71994360c848bda80289e809
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-04-03 09:37:43 +0200

    .env update on GitHub Actions

--- src/routes.ts
+++ src/routes.ts
@@ -29,7 +29,7 @@ const router: Router = Router();
  *         description: Server up and running.
  */
 router.get('/', (req: Request, res: Response) => {
-    res.status(200).send({ message: 'Welcome to the TypeScript MEN REST-API with changes' });
+    res.status(200).send({ message: 'Welcome to the TypeScript MEN REST-API' });
 })
 
 
