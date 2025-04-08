d2d953d1cea8fd920370b02e1162ffd04c72a3b8
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-04-08 17:50:38 +0200

    Pass workflow

--- src/repository/database.ts
+++ src/repository/database.ts
@@ -6,36 +6,36 @@ import mongoose from "mongoose";
  */
 export async function testConnection() {
 
-    try {
-        await connect();
-        await disconnect();
-        console.log("Database connection test successful (connect + disconnect).");
-    }
-    catch (error) {
-        console.log("Error testing database connection." + error);
-    }
+  try {
+    await connect();
+    await disconnect();
+    console.log("Database connection test successful (connect + disconnect).");
+  }
+  catch (error) {
+    console.log("Error testing database connection." + error);
+  }
 }
 
 /**
  * Connects to the db
  */
 export async function connect() {
-    try {
+  try {
 
-        if (!process.env.DBHOST) {
-            throw new Error("DBHOST environment variable is not defined");
-        }
-        await mongoose.connect(process.env.DBHOST);
-
-        if (mongoose.connection.db) {
-            await mongoose.connection.db.admin().command({ ping: 1 });
-        } else {
-            throw new Error("Database connection is not established.");
-        }
-
-    } catch (error) {
-        console.log("Error connecting to the database. Error: " + error);
+    if (!process.env.DBHOST) {
+      throw new Error("DBHOST environment variable is not defined");
     }
+    await mongoose.connect(process.env.DBHOST);
+
+    if (mongoose.connection.db) {
+      await mongoose.connection.db.admin().command({ ping: 1 });
+    } else {
+      throw new Error("Database connection is not established.");
+    }
+
+  } catch (error) {
+    console.log("Error connecting to the database. Error: " + error);
+  }
 }
 
 /**
@@ -43,11 +43,11 @@ export async function connect() {
  */
 export async function disconnect() {
 
-    try {
-        await mongoose.disconnect();
-        //console.log(mongoose.connections.length);
-    }
-    catch (error) {
-        console.log("Database connection closed." + error);
-    }
+  try {
+    await mongoose.disconnect();
+    //console.log(mongoose.connections.length);
+  }
+  catch (error) {
+    console.log("Database connection closed." + error);
+  }
 }
--- tests/product.test.ts
+++ tests/product.test.ts
@@ -2,92 +2,92 @@ import { test, expect } from "@playwright/test";
 
 export default function productTestCollection() {
 
-    /**
+  /**
      * 
      */
-    test("Workflow - register, login, create product and verify", async ({ request }) => {
+  test("Workflow - register, login, create product and verify", async ({ request }) => {
 
-        test.setTimeout(30_000); // Increase test timeout due to MongoDB Atlas being slow at times.
+    test.setTimeout(30_000); // Increase test timeout due to MongoDB Atlas being slow at times.
 
-        //------------------------------------------------------------------------------
-        // Create test objects
-        //------------------------------------------------------------------------------
-        const userReg = {
-            name: "Lars Larsen",
-            email: "mail@larsen.com",
-            password: "12345678"
-        }
+    //------------------------------------------------------------------------------
+    // Create test objects
+    //------------------------------------------------------------------------------
+    const userReg = {
+      name: "Lars Larsen",
+      email: "mail@larsen.com",
+      password: "12345678"
+    }
 
-        const userLogin = {
-            email: "mail@larsen.com",
-            password: "12345678"
-        }
+    const userLogin = {
+      email: "mail@larsen.com",
+      password: "12345678"
+    }
 
-        //------------------------------------------------------------------------------
-        // Register user
-        //------------------------------------------------------------------------------
-        let response = await request.post("/api/user/register", { data: userReg });
-        let json = await response.json();
+    //------------------------------------------------------------------------------
+    // Register user
+    //------------------------------------------------------------------------------
+    let response = await request.post("/api/user/register", { data: userReg });
+    let json = await response.json();
 
-        //console.log("Register:" + json);
+    //console.log("Register:" + json);
 
-        //expect(response.ok()).toBeTruthy();
-        //expect(result).toHaveLength(0);
-        expect(response.status()).toBe(200);
-
-
-        //------------------------------------------------------------------------------
-        // Login user
-        //------------------------------------------------------------------------------
-        response = await request.post("/api/user/login", { data: userLogin });
-        json = await response.json();
-
-        const token = json.data.token;
-        const userId = json.data.userId;
-        expect(response.status()).toBe(200);
+    //expect(response.ok()).toBeTruthy();
+    //expect(result).toHaveLength(0);
+    expect(response.status()).toBe(200);
 
 
+    //------------------------------------------------------------------------------
+    // Login user
+    //------------------------------------------------------------------------------
+    response = await request.post("/api/user/login", { data: userLogin });
+    json = await response.json();
 
-        //------------------------------------------------------------------------------
-        // Create product
-        //------------------------------------------------------------------------------
-        const expectedProduct =
-        {
-            "name": "Mr. Burns statue",
-            "description": "The best and most precious",
-            "imageURL": "https://picsum.photos/500/500",
-            "price": 100.96,
-            "stock": 15,
-            "discount": true,
-            "discountPct": 25,
-            "isHidden": false,
-            "_createdBy": userId
-        }
-
-        response = await request.post("/api/products/", {
-            data: expectedProduct,
-            headers: {
-                "auth-token": token,
-            }
-        });
-
-        expect(response.status()).toBe(201);
+    const token = json.data.token;
+    const userId = json.data.userId;
+    expect(response.status()).toBe(200);
 
 
 
-        //------------------------------------------------------------------------------
-        // Verify we have one product in the test repository
-        //------------------------------------------------------------------------------
-        response = await request.get("/api/products/");
-        json = await response.json();
-        const receivedProduct = json[0];
+    //------------------------------------------------------------------------------
+    // Create product
+    //------------------------------------------------------------------------------
+    const expectedProduct =
+    {
+      "name": "Mr. Burns statue",
+      "description": "The best and most precious",
+      "imageURL": "https://picsum.photos/500/500",
+      "price": 100.96,
+      "stock": 15,
+      "discount": true,
+      "discountPct": 25,
+      "isHidden": false,
+      "_createdBy": userId
+    }
 
-        //console.log(json) // output receivedProduct
-        // verify product data
-        expect(receivedProduct.name).toEqual(expectedProduct.name);
-        expect(receivedProduct.description).toEqual(expectedProduct.description);
-
-        expect(json).toHaveLength(1);
-        
+    response = await request.post("/api/products/", {
+      data: expectedProduct,
+      headers: {
+        "auth-token": token,
+      }
     });
+
+    expect(response.status()).toBe(201);
+
+
+
+    //------------------------------------------------------------------------------
+    // Verify we have one product in the test repository
+    //------------------------------------------------------------------------------
+    response = await request.get("/api/products/");
+    json = await response.json();
+    const receivedProduct = json[0];
+
+    //console.log(json) // output receivedProduct
+    // verify product data
+    expect(receivedProduct.name).toEqual(expectedProduct.name);
+    expect(receivedProduct.description).toEqual(expectedProduct.description);
+
+    expect(json).toHaveLength(1);
+
+  });
 }
