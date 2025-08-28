ea33a21a56bdd1a9a8580dcb25b8b347acd4f298
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-08-28 10:59:04 +0200

    Update swagger and dataSeeder.ts

--- src/util/docManager.ts
+++ src/util/docManager.ts
@@ -6,7 +6,7 @@ export function setupDocs(app: Application) {
   const swaggerDefinition = {
     openapi: '3.0.0',
     info: {
-      title: 'Express API for JSONPlaceholder - just updated',
+      title: 'MENTS-RESTAPI',
       version: '1.0.0',
       description:
         'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
